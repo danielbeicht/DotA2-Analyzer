121fe643af29bcd8af5c1a0d486779e3cafc8f9f
Daniel <danielbeicht@mz-host.de>
2015-12-25 02:46:49 +0100

    - redesigned all hero arrays (every hero object only exists once (call by reference) - implemented hero pick -> can't pick one hero multiple times - implemented delete heroes by clicking hero image - implemented default unselected hero image - removed normalized advantage - code improvements

--- app/components/home/home.ctrl.js
+++ app/components/home/home.ctrl.js
@@ -14,41 +14,114 @@
     function homeCtrl($scope, $http) {
         initHome($scope, $http);
 
+
+        $scope.yourTeamHeroPick = function(){
+            if ($scope.selectedHeroYourTeam != null){
+                var selectedHero = $scope.heroesSortedByIndex[$scope.selectedHeroYourTeam];
+                var heroAlreadyPicked = false;
+
+                for (var i=0; i<5; i++){
+                    if (($scope.yourTeamPicks[i] != null && $scope.yourTeamPicks[i].heroIndex === selectedHero.heroIndex) || ($scope.enemyTeamPicks[i] != null && $scope.enemyTeamPicks[i].heroIndex === selectedHero.heroIndex)){
+                        heroAlreadyPicked = true;
+                        // TODO: POPUP Held wurde bereits gewählt
+                    }
+                }
+
+                if (!heroAlreadyPicked){
+                    var heroPicked = false;
+                    for (var i=0; i<5; i++){
+                        if ($scope.yourTeamPicks[i] == null){
+                            $scope.yourTeamPicks[i] = $scope.heroesSortedByIndex[$scope.selectedHeroYourTeam];
+                            heroPicked = true;
+                            break;
+                        }
+                    }
+                    if (heroPicked == false){
+                        // TODO: POPUP Alle 5 Helden bereits gewählt
+                    }
+                }
+            }
+
+        }
+
+        $scope.enemyTeamHeroPick = function(){
+            if ($scope.selectedHeroEnemyTeam != null) {
+                var selectedHero = $scope.heroesSortedByIndex[$scope.selectedHeroEnemyTeam];
+                var heroAlreadyPicked = false;
+
+                for (var i=0; i<5; i++){
+                    if (($scope.yourTeamPicks[i] != null && $scope.yourTeamPicks[i].heroIndex === selectedHero.heroIndex) || ($scope.enemyTeamPicks[i] != null && $scope.enemyTeamPicks[i].heroIndex === selectedHero.heroIndex)){
+                        heroAlreadyPicked = true;
+                        // TODO: POPUP Held wurde bereits gewählt
+                    }
+                }
+
+                if (!heroAlreadyPicked) {
+                    var heroPicked = false;
+                    for (var i = 0; i < 5; i++) {
+                        if ($scope.enemyTeamPicks[i] == null) {
+                            $scope.enemyTeamPicks[i] = $scope.heroesSortedByIndex[$scope.selectedHeroEnemyTeam];
+                            heroPicked = true;
+                            break;
+                        }
+                    }
+                    if (heroPicked == false) {
+                        // TODO: POPUP schon 5 Helden gewählt
+                    }
+                }
+            }
+        }
     }
+
+    // Sort heroes by Index
+    function compare(a,b) {
+        if (parseInt(a.heroIndex) < parseInt(b.heroIndex))
+            return -1;
+        if (parseInt(a.heroIndex) > parseInt(b.heroIndex))
+            return 1;
+        //return 0;
+    }
+
+
 
     function initHome($scope, $http) {
         $scope.showSpinner = true;
 
+
+
+        var templateHero = {pic: 'h'}
+        $scope.yourTeamPicks = new Array(5);
+        $scope.enemyTeamPicks = new Array(5);
         // Get all heroes
         $http({
             method: 'GET',
             url: 'http://rest.mz-host.de:5015/DotAREST/webresources/heroes'
         }).then(function successCallback(response) {
-            var heroes = response.data.hero;
+            var heroesTemp = response.data.hero;
 
-            // Erstelle zwei Arrays für beide Tabellen (initialisiere anfangs mit 0)
-            $scope.yourTeamHeroData = new Array(heroes.length);
-            $scope.enemyTeamHeroData = new Array(heroes.length);
-
-            // need deep copy here else pointing to same object later
-            for (var i = 0; i < heroes.length; i++) {
-                var heroYourTeam = angular.copy(heroes[i]);
-                var heroEnemyTeam = angular.copy(heroes[i]);
-                var pic = 'assets/images/heroes/' + heroes[i].heldFullName.trim().replace(/\s/gi, "_") + '.jpg';
-                $scope.yourTeamHeroData[i] = {hero: heroYourTeam, winrate: 0, advantage: 0, normalizedAdvantage: 0, pic: pic};
-                $scope.enemyTeamHeroData[i] = {hero: heroEnemyTeam, winrate: 0, advantage: 0, normalizedAdvantage: 0, pic: pic};
+            $scope.heroes = new Array (heroesTemp.length);
+            for (var i = 0; i < $scope.heroes.length; i++) {
+                $scope.heroes[i] = {heroIndex: heroesTemp[i].heldIndex, heroID: heroesTemp[i].heldID, heroFullName:heroesTemp[i].heldFullName, heroName: heroesTemp[i].heldName,
+                    heroImageURL: 'assets/images/heroes/' + heroesTemp[i].heldFullName.trim().replace(/\s/gi, "_") + '.jpg', yourTeamAdvantage: 0, enemyTeamAdvantage: 0, yourTeamWinrate: '-%', enemyTeamWinrate: '-%'};
             }
-            $scope.showSpinner = false;
 
 
-            $scope.sortTypeYourTeam = 'hero.heldFullName'; // set the default sort type
+            $scope.sortTypeYourTeam = 'heroFullName'; // set the default sort type
             $scope.sortReverseYourTeam = false;  // set the default sort order
 
-            $scope.sortTypeEnemyTeam = 'hero.heldFullName'; // set the default sort type
+            $scope.sortTypeEnemyTeam = 'heroFullName'; // set the default sort type
             $scope.sortReverseEnemyTeam = false;  // set the default sort order
 
             $scope.searchHeroYourTeam = '';     // set the default search/filter term
             $scope.searchHeroEnemyTeam = '';
+
+
+            // Create Array to select heroes by Index in O(1) (references)
+            $scope.heroesSortedByIndex = new Array($scope.heroes.length);
+            for (var i=0; i<$scope.heroes.length; i++){
+                $scope.heroesSortedByIndex[i] = $scope.heroes[i];
+            }
+            $scope.heroesSortedByIndex.sort(compare);
 
             // this callback will be called asynchronously
             // when the response is available
@@ -77,4 +150,6 @@
             {text: "secondary message...", type: "secondary"}
         ];
     }
+
+
 })();
